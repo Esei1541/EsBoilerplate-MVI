310c9709c4a2e2b61dd4152795c00780115a9160
Esei <esei.public@gmail.com>
2026-04-11 07:25:06 +0900

    build: Maven Repository 배포 설정

--- esboilerplatemvi/build.gradle.kts
+++ esboilerplatemvi/build.gradle.kts
@@ -1,6 +1,41 @@
 plugins {
     id("com.android.library")
+    id("com.vanniktech.maven.publish") version "0.36.0"
     alias(libs.plugins.compose.compiler)
+}
+
+mavenPublishing {
+    coordinates("io.github.esei1541", "esboilerplate-mvi", "0.0.1")
+
+    pom {
+        name.set("esboilerplate-mvi")
+        description.set("A boilerplate library for Android MVI architecture with Jetpack Compose.")
+        inceptionYear.set("2026")
+        url.set("https://github.com/Esei1541/EsBoilerplate-MVI")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("Esei1541")
+                name.set("Seungjin Choi")
+                email.set("esei.public@gmail.com")
+                url.set("https://github.com/Esei1541")
+            }
+        }
+        scm {
+            url.set("https://github.com/Esei1541/EsBoilerplate-MVI")
+            connection.set("scm:git:github.com/Esei1541/EsBoilerplate-MVI.git")
+            developerConnection.set("scm:git:ssh://github.com/Esei1541/EsBoilerplate-MVI.git")
+        }
+    }
+
+    publishToMavenCentral()
+    signAllPublications()
 }
 
 android {
